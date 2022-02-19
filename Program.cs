c1384260d639c05fbd504b1a09d39f8ee7d71ce3
HP <HP@DESKTOP-POHI4N8>
2022-02-19 14:15:34 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace arrayy
+{
+    class arraprogram2
+    {
+        static void Main()
+        {
+            int num=1;
+            int i;
+            int fact=1;
+            for (i = 0; i <= num; i++)
+            {
+                fact = num * i;
+               
+
+            }
+            Console.WriteLine("Enter Any number: " + fact);
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+
+
+    }
+        
+        
+        
+        
+}
